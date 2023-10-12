b06034b7726a1c99d028bbba225e8cd2484780b7
Romain Ruetschi <106849+romac@users.noreply.github.com>
2023-10-12 12:06:10 +0200

    Fix glue code

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,9 +1,8 @@
+#![allow(unused_imports)]
+
 pub mod contract;
 
-use contract::{
-    ibc_transfer::ContractStorage,
-    msg::{ExecuteMsg, ExecuteMsg_Send, InstantiateMsg},
-};
+use contract::ibc_transfer::{ContractStorage, ExecuteMsg_Send, InstantiateMsg};
 
 use cosmwasm_std::{
     entry_point, DepsMut, Env, MessageInfo, Reply, Response, StdError, StdResult, Storage,
@@ -13,6 +12,10 @@ use serde::{de::DeserializeOwned, Serialize};
 
 const STORAGE_KEY: &[u8] = b"storage";
 
+pub enum ExecuteMsg {
+    Send(ExecuteMsg_Send),
+}
+
 #[entry_point]
 pub fn instantiate(
     deps: DepsMut,
@@ -21,7 +24,7 @@ pub fn instantiate(
     msg: InstantiateMsg,
 ) -> StdResult<Response> {
     let initial_storage = ContractStorage::default();
-    let (result, storage) = contract::ibc_transfer::instantiate_helper(initial_storage, info, msg);
+    let (result, storage) = contract::ibc_transfer::instantiate(initial_storage, info, msg);
     let result = StdResult::from(result)?;
 
     save(deps.storage, STORAGE_KEY, &storage)?;
@@ -47,8 +50,7 @@ pub fn execute_send(
     msg: ExecuteMsg_Send,
 ) -> StdResult<Response<NeutronMsg>> {
     let initial_storage = load::<ContractStorage>(deps.storage, STORAGE_KEY)?;
-    let (result, storage) =
-        contract::ibc_transfer::execute_send_helper(info, env, msg, initial_storage);
+    let (result, storage) = contract::ibc_transfer::execute_send(info, env, msg, initial_storage);
 
     let messages = StdResult::from(result)?;
 
@@ -64,7 +66,7 @@ pub fn execute_send(
 #[entry_point]
 pub fn reply(deps: DepsMut, env: Env, msg: Reply) -> StdResult<Response> {
     let initial_storage = load::<ContractStorage>(deps.storage, STORAGE_KEY)?;
-    let (result, storage) = contract::ibc_transfer::reply_helper(env, msg.into(), initial_storage);
+    let (result, storage) = contract::ibc_transfer::reply(env, msg.into(), initial_storage);
     let result = StdResult::from(result)?;
 
     save(deps.storage, STORAGE_KEY, &storage)?;
